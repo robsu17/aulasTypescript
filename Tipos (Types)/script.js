3e20ee20cbf97dffc3f2da19b86599112fb52096
robsu17 <robson.costa134@gmail.com>
2023-06-15 11:00:48 -0300

    index update

---
+++ Tipos (Types)/script.js
@@ -0,0 +1,8 @@
+const frase = "Front End"
+const total = 100.05
+
+frase.toLowerCase()
+// total.toLowerCase()
+
+const t = total.toFixed() // Método toFixed() retorna uma string
+console.log(t + 10) // Retorna 10010 por que concatena uma string com o numero "100" + "10"
